2d488458f45345976b109b1317ab0a98ade0c91f
devbett <chbett@kabarak.ac.ke>
2021-09-09 13:45:24 +0300

    Stansand

---
+++ android/app/src/main/kotlin/com/example/stansand_african_limited/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stansand_african_limited
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login/dashboard_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_login/theme.dart';
+import 'package:flutter_login/widgets.dart';
+import 'package:stansand_african_limited/widgets/animated_numeric_text.dart';
+import 'package:stansand_african_limited/widgets/fade_in.dart';
+import 'package:stansand_african_limited/widgets/round_button.dart';
+import 'transition_route_observer.dart';
+import 'constants.dart';
+
+class DashboardScreen extends StatefulWidget {
+  static const routeName = '/dashboard';
+
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen>
+    with SingleTickerProviderStateMixin, TransitionRouteAware {
+  Future<bool> _goToLogin(BuildContext context) {
+    return Navigator.of(context)
+        .pushReplacementNamed('/')
+        // we dont want to pop the screen, just replace it completely
+        .then((_) => false);
+  }
+
+  final routeObserver = TransitionRouteObserver<PageRoute?>();
+  static const headerAniInterval = Interval(.1, .3, curve: Curves.easeOut);
+  late Animation<double> _headerScaleAnimation;
+  AnimationController? _loadingController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  AppBar _buildAppBar(ThemeData theme) {
+    final menuBtn = IconButton(
+      color: theme.accentColor,
+      icon: const Icon(FontAwesomeIcons.bars),
+      onPressed: () {},
+    );
+    final signOutBtn = IconButton(
+      icon: const Icon(FontAwesomeIcons.signOutAlt),
+      color: theme.accentColor,
+      onPressed: () => _goToLogin(context),
+    );
+    final title = Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Hero(
+              tag: Constants.logoTag,
+              child: Image.asset(
+                'assets/images/ecorp.png',
+                filterQuality: FilterQuality.high,
+                height: 30,
+              ),
+            ),
+          ),
+          HeroText(
+            Constants.appName,
+            tag: Constants.titleTag,
+            viewState: ViewState.shrunk,
+            style: LoginThemeHelper.loginTextStyle,
+          ),
+          SizedBox(width: 20),
+        ],
+      ),
+    );
+
+    return AppBar(
+      leading: FadeIn(
+        controller: _loadingController,
+        offset: .3,
+        curve: headerAniInterval,
+        fadeDirection: FadeDirection.startToEnd,
+        child: menuBtn,
+      ),
+      actions: <Widget>[
+        FadeIn(
+          controller: _loadingController,
+          offset: .3,
+          curve: headerAniInterval,
+          fadeDirection: FadeDirection.endToStart,
+          child: signOutBtn,
+        ),
+      ],
+      title: title,
+      backgroundColor: theme.primaryColor.withOpacity(.1),
+      elevation: 0,
+      textTheme: theme.accentTextTheme,
+      iconTheme: theme.accentIconTheme,
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return WillPopScope(
+      onWillPop: () => _goToLogin(context),
+      child: SafeArea(
+        child: Scaffold(
+          body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: theme.primaryColor.withOpacity(.1),
+            child: Container()
+          ),
+        ),
+      ),
+    );
+  }
+}
